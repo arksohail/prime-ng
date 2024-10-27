7c715e0cca03cd600cf592cff500cf9a8931be77
arksohail <arksohail@gmail.com>
2024-10-27 20:15:14 +0530

    Added Routes to Lazy Load Prime-NG Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'prime-ng',
+    pathMatch: 'full'
+  },
+  {
+    path: 'prime-ng',
+    loadComponent: () => import('./prime-ng/prime-ng.component').then(m => m.PrimeNgComponent)
+  }
+];
